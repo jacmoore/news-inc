99330f4cb2df75349ac8579df6fa4e11ff1ae46c
Jacinta Moore <jacintamoore@Jacintas-MacBook-Pro.local>
2024-12-10 10:59:03 +0000

    fetch and show story title

--- src/app/data/stories.service.ts
+++ src/app/data/stories.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from "@angular/common/http";
 import { inject, Injectable } from "@angular/core";
-import { Observable } from "rxjs";
+import { first, forkJoin, Observable, tap } from "rxjs";
+import { Story } from "./story";
 
 @Injectable({
   providedIn: "root",
@@ -8,9 +9,51 @@ import { Observable } from "rxjs";
 export class StoriesService {
   #http = inject(HttpClient);
 
-  find(): Observable<number[]> {
-    return this.#http.get<number[]>(
-      `https://hacker-news.firebaseio.com/v0/topstories.json`
+  allStoryIds: number[] = [];
+  stories: Story[] = [];
+  fetchingStoryDetails = false;
+  page = 0;
+
+  PAGE_SIZE = 6;
+  HACKER_NEWS_API_URL = "https://hacker-news.firebaseio.com/v0";
+
+  fetch(): Observable<number[]> {
+    return this.#http
+      .get<number[]>(`${this.HACKER_NEWS_API_URL}/topstories.json`)
+      .pipe(first());
+  }
+
+  fetchStories(storyIds: number[]): Observable<Story[]> {
+    const ids = this.getStoryIdsForCurrentPage(storyIds);
+    this.setFetchingStoryDetails(true);
+
+    return this.fetchStoryDetails(ids).pipe(
+      tap(() => {
+        this.setFetchingStoryDetails(false);
+        this.incrementPage();
+      })
     );
+  }
+
+  private getStoryIdsForCurrentPage(storyIds: number[]): number[] {
+    const start = this.page * this.PAGE_SIZE;
+    const end = start + this.PAGE_SIZE;
+    return storyIds.slice(start, end);
+  }
+
+  private setFetchingStoryDetails(value: boolean): void {
+    this.fetchingStoryDetails = value;
+  }
+
+  private fetchStoryDetails(ids: number[]): Observable<Story[]> {
+    return forkJoin(
+      ids.map((id: number) =>
+        this.#http.get<Story>(`${this.HACKER_NEWS_API_URL}/item/${id}.json`)
+      )
+    ).pipe(first());
+  }
+
+  private incrementPage(): void {
+    this.page++;
   }
 }
