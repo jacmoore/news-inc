542ee324f3e2a767d6dc490a9273b12072b64c38
Jacinta Moore <jacintamoore@Jacintas-MacBook-Pro.local>
2024-12-10 09:39:32 +0000

    added stories to app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from "@angular/core";
+import { RouterOutlet } from "@angular/router";
+import { StoriesComponent } from "./stories/stories.component";
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  selector: "app-root",
+  imports: [RouterOutlet, StoriesComponent],
+  templateUrl: "./app.component.html",
+  styleUrl: "./app.component.css",
 })
 export class AppComponent {
-  title = 'news-inc';
+  title = "news-inc";
 }
