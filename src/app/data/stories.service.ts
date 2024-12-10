3777ecc9f6793ffdef0e4166fbf6da5bfa387a00
Jacinta Moore <jacintamoore@Jacintas-MacBook-Pro.local>
2024-12-10 09:58:09 +0000

    get stories and show on UI

--- src/app/data/stories.service.ts
+++ src/app/data/stories.service.ts
@@ -1,9 +1,16 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { inject, Injectable } from "@angular/core";
+import { Observable } from "rxjs";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class StoriesService {
+  #http = inject(HttpClient);
 
-  constructor() { }
+  find(): Observable<number[]> {
+    return this.#http.get<number[]>(
+      `https://hacker-news.firebaseio.com/v0/topstories.json`
+    );
+  }
 }
