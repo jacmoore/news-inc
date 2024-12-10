4f05f1a73068f88886397512628428159df6580a
Jacinta Moore <jacintamoore@Jacintas-MacBook-Pro.local>
2024-12-10 09:49:34 +0000

    added stories service and Story type

---
+++ src/app/data/story.ts
@@ -0,0 +1,11 @@
+export type Story = {
+  id: number;
+  by: string;
+  descendants: number;
+  kids: number[];
+  score: number;
+  time: number;
+  title: string;
+  type: string;
+  url: string;
+};
